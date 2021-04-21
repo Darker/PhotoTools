bb67586db8edf9b65424aa6e299fcca6ef860fe2
Jakub Mareda <jmareda@seznam.cz>
2021-04-21 10:05:01 +0200

    Also redirect stderr to log

--- FileTools/FileTools/batch/makeHalfResolutionPhoto.bat
+++ FileTools/FileTools/batch/makeHalfResolutionPhoto.bat
@@ -1,1 +1,1 @@
-node --experimental-modules %~dp0..\DownscalePhoto.js %1 0.5 > %~dp0MakeHalfResolutionPhoto.bat.log
+node --experimental-modules %~dp0..\DownscalePhoto.js %1 0.5 1> %~dp0MakeHalfResolutionPhoto.bat.log 2>&1
